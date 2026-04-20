66181a1b6fc5df4f18e440b61342cc0d2a8f3cd9
elyasgo30you-stack <elyasgo30you@gmail.com>
2026-04-21 01:23:50 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/PlayerObject.hpp>
 
+using namespace geode::prelude;
+
+class $modify(PlayLayer) {
+    void destroyPlayer(PlayerObject* p0, GameObject* p1) {
+        if (Mod::get()->getSettingValue<bool>("noclip") || Mod::get()->getSettingValue<bool>("godmode")) return;
+        PlayLayer::destroyPlayer(p0, p1);
+    }
+};
+
+class $modify(PlayerObject) {
+    void pushButton(PlayerButton p0) {
+        if (Mod::get()->getSettingValue<bool>("inf-jump")) this->m_isOnGround = true;
+        PlayerObject::pushButton(p0);
+    }
+};
